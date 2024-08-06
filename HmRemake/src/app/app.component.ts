1635e07bdc5cc9150b0b7fcbcebec6b19edf23f7
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-08-06 19:58:30 -0300

    Adicionado header e index

--- HmRemake/src/app/app.component.ts
+++ HmRemake/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent} from './header/header.component';
+import { HomeComponent
 
+ } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   template: `
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
   <app-header></app-header>
+  <app-home></app-home>
   `,
   styleUrl: './app.component.css'
 })
