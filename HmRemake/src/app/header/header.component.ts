34bcf8d39240aee41ff46a6a2d948a2d8c64bcef
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-08-27 19:20:45 -0300

    Revert "falha ao criar header"
    This reverts commit 5965ceb83945477ac20cad0ea876d339948339a8.

--- HmRemake/src/app/header/header.component.ts
+++ HmRemake/src/app/header/header.component.ts
@@ -1,25 +1,13 @@
-import { Component, OnInit} from '@angular/core';
-import { NgbCollapseModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink, RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from "@angular/material/toolbar"; 
-import {MatSidenavModule} from "@angular/material/sidenav"; 
-import {MatList, MatListModule} from "@angular/material/list"; 
-import {MatButtonModule} from "@angular/material/button"; 
-import {MatIconModule} from "@angular/material/icon";
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgbNavModule, RouterLink, RouterModule, CollapseModule],
+  imports: [NgbNavModule, RouterLink, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-
 export class HeaderComponent {
-
-
   active = 1;
-  isCollapsed = true;
 }
